581028fb71910fe7ea82e95a79835d9971b1f6a0
somuip <117620797+somuip@users.noreply.github.com>
2023-02-20 10:42:49 +0530

    solution 2

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -7,11 +7,10 @@ public class Pizza {
     private String bill;
 
     private int toppings;
-
-    boolean isExtraCheeseAdded;
-    boolean isExtraToppingAdded;
-    boolean isBillCreated;
-    boolean isTakeAway;
+    boolean isExtracheese;
+    boolean isExtratopping;
+    boolean isBill;
+    boolean bag;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
@@ -19,19 +18,10 @@ public class Pizza {
         if(isVeg){
             this.price = 300;
             this.toppings = 70;
-        }
-        else{
+        }else{
             this.price = 400;
             this.toppings = 120;
         }
-
-        // optional do for good practice
-        isBillCreated = false;
-        isExtraCheeseAdded = false;
-        isExtraToppingAdded = false;
-        isTakeAway = false;
-
-        // the base price will be set here only
         this.bill = "Base Price Of The Pizza: "+this.price+"\n";
     }
 
@@ -40,53 +30,46 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-
-        // check if already cheese added or not
-        if(!isExtraCheeseAdded){
-            this.price += 80;
-            isExtraCheeseAdded = true; // only once we have to add so
+        // your code goes here
+        if(!isExtracheese){
+            this.price +=80;
+            isExtracheese = true;
         }
+
     }
 
     public void addExtraToppings(){
-
-        // check if already toppings added or not
-        if(!isExtraToppingAdded){
+        // your code goes here
+        if(!isExtratopping){
             this.price += this.toppings;
-            isExtraToppingAdded = true; // only once we have to add so
+            isExtratopping = true;
         }
     }
 
     public void addTakeaway(){
-
-        // check if takeaway is added or not
-        if(!isTakeAway){
-            this.price += 20;
-            isTakeAway = true; // only once we have to add so
+        // your code goes here
+        if(!bag){
+            this.price+=20;
+            bag = true;
         }
     }
 
     public String getBill(){
-
-        if(!isBillCreated){
-            if(isExtraCheeseAdded){
-                this.bill += "Extra Cheese Added: 80"+"\n";
+        // your code goes here
+        if(!isBill) {
+            if (isExtracheese) {
+                this.bill += "Extra Cheese Added: 80" + "\n";
             }
-
-            if(isExtraToppingAdded){
-                this.bill += "Extra Toppings Added: "+this.toppings+"\n";
+            if (isExtratopping) {
+                this.bill += "Extra Toppings Added: " + this.toppings + "\n";
             }
-
-            if(isTakeAway){
-                this.bill += "Paperbag Added: 20"+"\n";
+            if (bag) {
+                this.bill += "Paperbag Added: 20" + "\n";
             }
-
-            this.bill += "Total Price: "+this.price+"\n";
-
-            isBillCreated = true; // to avoid making multiple bills
-
-            return this.bill+"\n";
+            this.bill += "Total Price: " + this.price+"\n";
+            this.isBill = true;
+            return this.bill;
         }
-        return
+        return this.bill;
     }
 }
